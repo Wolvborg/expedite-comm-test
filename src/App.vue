33c6751c5df8e0e6e8c0e3e52ab54f75326b6186
Abhishek Saini <binnu24668@gmail.com>
2024-09-28 21:42:33 +0530

    Added provide-inject

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,10 @@
 </template>
 
 <script setup>
+  import { provide } from 'vue';
+
   import MetadataRenderer from './components/MetadataRenderer.vue';
+
+  const textColor = 'text-green-500';
+  provide('text-color', textColor);
 </script>
--- src/components/ParagraphComponent.vue
+++ src/components/ParagraphComponent.vue
@@ -1,13 +1,17 @@
 <template>
-  <p v-for="(para, index) in paras" :key="index" class="mb-4">
-    {{ para }}
+  <p v-for="(para, index) in paras" :key="index" :class="['mb-4', { [textColor]: para.dataFlow }]">
+    {{ para.text }}
   </p>
 </template>
 
 <script setup>
+  import { inject } from 'vue';
+
   defineProps({
     paras: {
       required: true,
     },
   });
+
+  const textColor = inject('text-color');
 </script>
